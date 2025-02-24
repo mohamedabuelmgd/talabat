76c75334f924e313fe0ec686b160616932b4254e
mohamedabuelmgd <mohammedabuelmgd@gmail.com>
2025-02-24 17:50:46 +0200

    Add files via upload

---
+++ Talabate.Sevice/OrderService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories;
+using Talabat.Core.Services;
+using Talabat.Core.Specification;
+
+namespace Talabate.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPaymentService _paymentService;
+
+        public OrderService(IBasketRepository basketRepository
+            , IUnitOfWork unitOfWork
+            ,IPaymentService paymentService)
+        {
+            _basketRepository = basketRepository;
+            _unitOfWork = unitOfWork;
+            _paymentService = paymentService;
+        }
+        public async Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int deliveryMethodId, Address shippingAddress)
+        {
+            //1.get basket by basket id
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+            //2.get the item that iwant to put in the basket
+            var orderItems = new List<OrderItem>();
+            foreach (var item in basket.Items)
+            {
+                var product = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                var productItemOrdered = new ProductItemOrder(product.Id, product.Name, product.PictureUrl);
+                var orderItem = new OrderItem(productItemOrdered, product.Price, item.Quantity);
+                orderItems.Add(orderItem);
+            }
+            //3. sum the total of the price 
+
+            var subTotal = orderItems.Sum(item => item.Price * item.Quantity);
+            //4. get delivery methods from delivery repo by unite of work
+            var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(deliveryMethodId);
+
+            //befor create order check if there is an order have the same payment intent that i hav
+            var spec = new OrderByPaymentIntentIdSpecification(basket.PaymentIntentId);
+            var existingOrder = await _unitOfWork.Repository<Order>().GetByIdWithSpecAsync(spec);
+            //if ther are order that have the same payment intent delete it and update the payment intent
+            if(existingOrder!=null)
+            {
+                _unitOfWork.Repository<Order>().Delete(existingOrder);
+                await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+            }
+
+
+            //5.create order
+            var order = new Order(buyerEmail, shippingAddress, deliveryMethod, orderItems, subTotal,basket.PaymentIntentId);
+            await _unitOfWork.Repository<Order>().CreateAsync(order);
+            //6.save change in database
+            var result =await _unitOfWork.Complete();//return number of order that create 
+            if (result<=0) return null;//not create order
+            return order;
+        }
+
+        public async Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            var spec =new OrderWithItemsAndDeliveryMethodSpecifications(buyerEmail);
+            var orders = await _unitOfWork.Repository<Order>().GetAllWithSpecAsync(spec);
+            return orders;
+        }
+
+        public async Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            var deliveryMethods = await _unitOfWork.Repository<DeliveryMethod>().GetAllAsync();
+            return deliveryMethods;
+        }
+
+        public async Task<Order> GetOrderByIdForUserAsync(int orderId, string buyerEmail)
+        {
+            var spec = new OrderWithItemsAndDeliveryMethodSpecifications(buyerEmail,orderId);
+            var order = await _unitOfWork.Repository<Order>().GetByIdWithSpecAsync(spec);
+            return order;
+        }
+    }
+}
---
+++ Talabate.Sevice/PaymentService.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories;
+using Talabat.Core.Services;
+using Talabat.Core.Specification;
+using Order = Talabat.Core.Entities.Order_Aggregate.Order;
+using Product = Talabat.Core.Entities.Product;
+
+namespace Talabate.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IBasketRepository _basketRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PaymentService(IConfiguration configuration
+            ,IBasketRepository basketRepository
+            ,IUnitOfWork unitOfWork)
+        {
+            _configuration = configuration;
+            _basketRepository = basketRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+            if (basket == null) return null;
+
+
+            var shippingPrice = 0m;
+            if(basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(basket.DeliveryMethodId.Value);
+                shippingPrice = deliveryMethod.Cost;
+                basket.ShippingPrice = deliveryMethod.Cost;
+            }
+
+            foreach (var item in basket.Items)//be sure that the price from front end is the same of database
+            {
+                var product = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                if (item.Price != product.Price)
+                    item.Price = product.Price;
+            }
+
+            var service = new PaymentIntentService();
+            PaymentIntent intent;
+
+            if(string.IsNullOrEmpty(basket.PaymentIntentId))//create payment intent
+            {
+                var options = new PaymentIntentCreateOptions()
+                {
+                    Amount=(long)basket.Items.Sum(item=> item.Quantity*item.Price*100)+(long) shippingPrice*100,
+                    Currency="usd",
+                    PaymentMethodTypes=new List<string>() { "card"}
+
+                };
+                intent=await service.CreateAsync(options);//intent have intent id and clientsecret key
+
+                basket.PaymentIntentId = intent.Id;
+                basket.ClientSecret = intent.ClientSecret;
+            }
+            else//update Payment intent
+            {
+                var options = new PaymentIntentUpdateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(item => item.Quantity * item.Price * 100) + (long)shippingPrice * 100
+                };
+                await service.UpdateAsync(basket.PaymentIntentId,options);
+            }
+
+
+            await _basketRepository.UpdateBasketAsync(basket);//because the chang of payment id and client secret
+            return basket;
+        }
+
+        public async Task<Talabat.Core.Entities.Order_Aggregate.Order> UpdatePaymentIntentToSucceededOrFailed(string paymentIntentId, bool IsSucceeded)
+        {
+            var spec = new OrderByPaymentIntentIdSpecification(paymentIntentId);
+            var order = await _unitOfWork.Repository<Order>().GetByIdWithSpecAsync(spec);
+            if (IsSucceeded)
+                order.Status = OrderStatus.PaymentReceived;
+            else
+                order.Status = OrderStatus.PaymentFailed;
+
+            _unitOfWork.Repository<Order>().Update(order);
+            await _unitOfWork.Complete();
+            return order;
+        }
+    }
+}
